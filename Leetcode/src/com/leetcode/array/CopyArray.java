1026e5355cbdaee6140c2994940e0892c63dc2e4
thimy197 <daothimy197@gmail.com>
2021-10-23 23:25:34 +0700

    change format content of Array files

--- Leetcode/src/com/leetcode/array/CopyArray.java
+++ Leetcode/src/com/leetcode/array/CopyArray.java
@@ -14,17 +14,17 @@ public class CopyArray {
 			System.out.print(cloneArray[i]+" ");
 		}
 		System.out.println("\n");
-		
+
 		// Java program to demonstrate 
 		// cloning of multi-dimensional arrays
 		int intArray1[][] = {{1,2,3},{4,5}};
-        int cloneArray1[][] = intArray1.clone();
-        // will print false
-        System.out.println(intArray1 == cloneArray1);
-        // will print true as shallow copy is created
-        // i.e. sub-arrays are shared
-        System.out.println(intArray1[0] == cloneArray1[0]);
-        System.out.println(intArray1[1] == cloneArray1[1]);
+		int cloneArray1[][] = intArray1.clone();
+		// will print false
+		System.out.println(intArray1 == cloneArray1);
+		// will print true as shallow copy is created
+		// i.e. sub-arrays are shared
+		System.out.println(intArray1[0] == cloneArray1[0]);
+		System.out.println(intArray1[1] == cloneArray1[1]);
 	}
 
 }
--- Leetcode/src/com/leetcode/array/TwoSum.java
+++ Leetcode/src/com/leetcode/array/TwoSum.java
@@ -8,16 +8,16 @@ public class TwoSum {
 
 	public static void main(String[] args) {
 		// testcase
-//		int[] arr = new int[] {3,3};
-//		int target = 6;
+		//		int[] arr = new int[] {3,3};
+		//		int target = 6;
 		int[] arr = new int[] {2,7,11,15};
 		int target = 9;
 
 		long startTime = System.nanoTime();
 
 		// run code
-//		printArr(twoSum(arr, target));
-//		printArr(twoSumTwoHashMap(arr, target));
+		//		printArr(twoSum(arr, target));
+		//		printArr(twoSumTwoHashMap(arr, target));
 		printArr(twoSumOneHashMap(arr, target));
 
 		long endTime   = System.nanoTime();
@@ -54,24 +54,24 @@ public class TwoSum {
 		// In case there is no solution, we'll just return null
 		return null;
 	}
-	
+
 	// O(n) 
 	// 1. loop các value trong arr
 	// 2. Kiểm tra phần bù của value tương ứng xem có nằm trong hashMap không
 	// 2.1 Nếu có thì return cặp index hiện tại và index trong hashMah tương ứng
 	// 2.2 Nếu không put <value, index> vào hashMap
 	public static int[] twoSumOneHashMap(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
-            }
-            map.put(nums[i], i);
-        }
-        // In case there is no solution, we'll just return null
-        return null;
-    }
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < nums.length; i++) {
+			int complement = target - nums[i];
+			if (map.containsKey(complement)) {
+				return new int[] { map.get(complement), i };
+			}
+			map.put(nums[i], i);
+		}
+		// In case there is no solution, we'll just return null
+		return null;
+	}
 
 	public static void printArr(int[] arr) {
 		System.out.println(Arrays.toString(arr));
